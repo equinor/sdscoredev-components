22894ca7e3e86ec840f4a44f503cb4234cf65846
John Wennstrøm <john.wennstrom@bouvet.no>
2022-03-10 21:16:25 +0100

    fix: missing unique key

--- dist/components/DataTable/Filter/Filter.d.ts
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-import { FilterProps } from '../Filter';
-export declare const Filter: React.FC<FilterProps>;
--- dist/components/Form/Validation.d.ts
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { ValidationProps } from './Validation/types';
-/**
- * Validation plugin. Must be used together with `validationReducer`
- *
- * @param props `<ValidationProps>`
- * @returns `JSX.Element`
- */
-export declare const Validation: React.FC<ValidationProps>;
