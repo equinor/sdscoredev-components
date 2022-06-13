443bf4123d883c0920c6197f1d76c8b54e0d9721
John Wennstrøm <john.wennstrom@bouvet.no>
2022-06-13 14:50:21 +0200

    fix: add extra props for input

--- src/components/Form/inputFields/TextField.tsx
+++ src/components/Form/inputFields/TextField.tsx
@@ -1,31 +1,27 @@
-import React from 'react';
-import { Icon, TextField as EdsTextField } from '@equinor/eds-core-react';
-import { InputProps, withInput } from './withInput';
-import styled from 'styled-components';
+import React from "react";
+import { Icon, TextField as EdsTextField } from "@equinor/eds-core-react";
+import { InputProps, withInput } from "./withInput";
+import styled from "styled-components";
 
 const StyledTextField = styled(EdsTextField)`
-    input {
-        padding-top: 7px;
-        height: 37px;
-    }
-`
+  input {
+    padding-top: 7px;
+    height: 37px;
+  }
+`;
 export const TextField: React.FC<InputProps> = (props: any) => {
-    const { id, meta, value, onChange, error, disabled, onBlur, placeholder } = props;
+  const { value, error } = props;
 
-    return (
-        <StyledTextField
-            id={id}
-            meta={meta}
-            defaultValue={value || ''}
-            onChange={onChange}
-            onBlur={onBlur}
-            variant={error ? "error" : "default"}
-            helperText={error}
-            helperIcon={error && <Icon name="error_filled" title="Error" />}
-            disabled={disabled}
-            placeholder={placeholder}
-        />
-    );
+  return (
+    <StyledTextField
+      {...props}
+      label=""
+      defaultValue={value || ""}
+      variant={error ? "error" : "default"}
+      helperText={error}
+      helperIcon={error && <Icon name="error_filled" title="Error" />}
+    />
+  );
 };
 
 export default withInput()(TextField);
