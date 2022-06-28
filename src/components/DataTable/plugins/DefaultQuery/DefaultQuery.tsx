ba41bb6a807040787081b6f0d0f79d4dfc1b8382
John Wennstrøm <john.wennstrom@bouvet.no>
2022-06-28 12:59:09 +0200

    fix: also add cache for default query

--- src/components/DataTable/plugins/DefaultQuery/DefaultQuery.tsx
+++ src/components/DataTable/plugins/DefaultQuery/DefaultQuery.tsx
@@ -15,52 +15,55 @@ export const DefaultQuery: React.FC<DefaultQueryProps> = ({ state, dispatch }) =
             params.get('pageIndex') && dispatch({ type: 'SET_PAGE_INDEX', payload: params.get('pageIndex') });
             params.get('pageSize') && dispatch({ type: 'SET_PAGE_SIZE', payload: params.get('pageSize') });
         }
-    }
+    };
 
     const setInitialSorting = (params: URLSearchParams) => {
         if (!sortingInitialized.current) {
             params.get('orderBy') && dispatch({ type: 'SET_ORDER_BY', payload: params.get('orderBy') });
             params.get('desc') && dispatch({ type: 'SET_DIRECTION', payload: params.get('desc') });
         }
-    }
+    };
 
     useEffect(() => {
         const params = new URLSearchParams(location.search);
 
-        setInitialPagination(params)
-        setInitialSorting(params)
+        setInitialPagination(params);
+        setInitialSorting(params);
 
-        dispatch({ type: 'SET_QUERY', payload: params.toString()});
+        dispatch({ type: 'SET_SEARCH_STRING', payload: location.search });
 
-    }, [location])
+        dispatch({ type: 'SET_QUERY', payload: params.toString() });
+    }, [location]);
 
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
-       
+
         if (paginationInitialized.current) {
             params.set('pageSize', state.paginationReducer.pageSize);
             params.set('pageIndex', state.paginationReducer.pageIndex);
-            const url = [window.location.pathname, params.toString()].join('?')
-            navigate(url)
+            const url = [window.location.pathname, params.toString()].join('?');
+            navigate(url);
         }
 
         paginationInitialized.current = true;
-        
-    }, [state.paginationReducer])
+    }, [state.paginationReducer]);
 
     useEffect(() => {
         const params = new URLSearchParams(window.location.search);
-       
-        if (sortingInitialized.current && state.sortReducer.orderBy && typeof state.sortReducer.ascending === 'boolean') {
+
+        if (
+            sortingInitialized.current &&
+            state.sortReducer.orderBy &&
+            typeof state.sortReducer.ascending === 'boolean'
+        ) {
             params.set('orderBy', state.sortReducer.orderBy);
             params.set('desc', state.sortReducer.ascending);
-            const url = [window.location.pathname, params.toString()].join('?')
-            navigate(url)
+            const url = [window.location.pathname, params.toString()].join('?');
+            navigate(url);
         }
 
         sortingInitialized.current = true;
-        
-    }, [state.sortReducer])
+    }, [state.sortReducer]);
 
     return <></>;
-}
+};
