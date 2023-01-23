40cb264b08010a1be79f616a03da341165e9cc21
sindreeio <sindreeide.olsen@gmail.com>
2023-01-23 16:05:41 +0100

    fix: filter indicator size undefined

--- src/components/DataTable/plugins/Filter/Filter.tsx
+++ src/components/DataTable/plugins/Filter/Filter.tsx
@@ -15,10 +15,11 @@ export const Filter: React.FC<FilterProps> = ({ title, onClick, hideButton }) =>
                     <Button variant="ghost" onClick={onClick} data-cy="sdscc-DataTable-Button-filter">
                         {title}
                         <Icon name="filter_list" title="Show filter" size={24} />
-                        {new URLSearchParams(location.search) &&
-                            new URLSearchParams(location.search)?.get('filter') &&
-                            new URLSearchParams(location.search)?.get('filter') !== '' && (
-                                <Icon name="star_filled" size={16} color="#e4502b" />
+                        {new URLSearchParams(location?.search) &&
+                            new URLSearchParams(location?.search)?.get('filter') &&
+                            new URLSearchParams(location?.search)?.get('filter') !== undefined &&
+                            new URLSearchParams(location?.search)?.get('filter') !== '' && (
+                                <Icon name="star_filled" title="Filter active" color="#e4502b" />
                             )}
                     </Button>
                 </Tooltip>
